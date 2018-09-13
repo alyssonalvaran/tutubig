54d0af15d2dca731f915f6a73db8cd9c7ed47a96
Alysson Alvaran <alvaran.alysson@gmail.com>
2018-09-13 14:08:36 +0800

    Add test HTTP method
    - Add initial static folder
    - Add initial templates folder

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Tutubig"
+    return render_template("index.html")
+
+@app.route('/test/<user_input>', methods=['GET', 'POST'])
+def test(user_input):
+    output = 'test'
+    if request.method == 'POST':
+        output = 'POST'
+    else:
+        output = 'GET'
+    return output + ': ' + user_input
