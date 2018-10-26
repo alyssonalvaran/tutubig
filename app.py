682323c342f980483c3c7257634b26d05cb11a2b
Alysson Alvaran <alvaran.alysson@gmail.com>
2018-10-26 16:32:55 +0800

    Add test GET and POST methods

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
@@ -34,11 +35,17 @@ def home():
 def demo():
 	return render_template('demo.html')
 
-@app.route('/test/<user_input>', methods=['GET', 'POST'])
-def test(user_input):
-    output = 'test'
-    if request.method == 'POST':
-        output = 'POST'
-    else:
-        output = 'GET'
-    return output + ': ' + user_input
+@app.route('/api/get/', methods=['GET'])
+def get():
+	output = {
+		"Status": "200"
+	}
+	
+	return json.dumps(output)
+
+@app.route('/api/post/', methods=['POST'])
+def post():
+	output = request.get_json()
+	output["Status"] = "200"
+	
+	return json.dumps(output)
