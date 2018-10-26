789e5a9d3aa4f6f277e966e53ed369127fd6ceee
Alysson Alvaran <alvaran.alysson@gmail.com>
2018-10-26 13:43:26 +0800

    Add home page layout
    - Add scrollspy sections in home page
    - Add vscode in gitignore
    - Add Alysson Alvaran as author

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# tutubig
+# Tutubig
 
 ## Authors
 
+* [Alysson Alvaran](https://github.com/alyssonalvaran)
 * [Karl Andrew Gatdula](https://github.com/kagatdula)
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+	return render_template('index.html')
 
 @app.route('/test/<user_input>', methods=['GET', 'POST'])
 def test(user_input):
