c99d866712796e9036b28aa6996da283bbf234f9
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-01-17 20:48:28 +0800

    Update demo page
    - Remove sample charts
    - Add Semantic UI
    - Add sidebar
    - Add sidebar routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -33,6 +33,22 @@ def home():
 
 @app.route("/demo/")
 def demo():
+    return redirect(url_for('demo_dashboard'))
+
+@app.route("/demo-dashboard/")
+def demo_dashboard():
+	return render_template('demo_dashboard.html')
+
+@app.route("/demo-profile/")
+def demo_profile():
+	return render_template('demo.html')
+
+@app.route("/demo-accounts/")
+def demo_accounts():
+	return render_template('demo.html')
+
+@app.route("/demo-settings/")
+def demo_settings():
 	return render_template('demo.html')
 
 @app.route('/api/get/', methods=['GET'])
